e5cafcd4fe9f6061fac11b7871ff5a6c36d9b09b
Alex <alex.marhefka@gmail.com>
2018-02-03 15:43:38 -0500

    Add presidential variables and array

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
 var iceCream = ['pistachio', 'mint', 'vanilla'];
 
 iceCream.push('chocolate');
+
+var trump = {
+	firstName: 'Donald',
+	lastName: 'Trump',
+	termLength: 4,
+	party: 'Republican',
+	yearsOfPresidency: 1
+};
+
+var obama = {
+	firstName: 'Barack',
+	lastName: 'Obama',
+	termLength: 4,
+	party: 'Democrat',
+	yearsOfPresidency: 8
+};
+
+var bush2 = {
+	firstName: 'George W.',
+	lastName: 'Bush',
+	termLength: 4,
+	party: 'Republican',
+	yearsOfPresidency: 8
+};
+
+var clinton = {
+	firstName: 'Bill',
+	lastName: 'Clinton',
+	termLength: 4,
+	party: 'Democrat',
+	yearsOfPresidency: 8
+};
+
+var bush1 = {
+	firstName: 'George H. W.',
+	lastName: 'Bush',
+	termLength: 4,
+	party: 'Republican',
+	yearsOfPresidency: 4
+};
+
+var presidents = [trump, obama, bush2, clinton, bush1];
+
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
